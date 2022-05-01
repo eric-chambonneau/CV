bbbc81e8351985acd8f95591a30be2a68fb34772
eric-chambonneau <eric.chambonneau@gmail.com>
2022-05-01 22:06:01 +0200

    4init

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typed', {
+    strings: ['Coucou j apprends le code', 'Ce n est pas facile', 'c est cool'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
